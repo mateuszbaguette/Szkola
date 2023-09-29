2d9f119a3e1d3858b993bbf0c5dd18e483176119
Mateusz B <144439963+mateuszbaguette@users.noreply.github.com>
2023-09-29 09:35:13 +0200

    Update funkcje.sql

--- bazy-danych/funkcje.sql
+++ bazy-danych/funkcje.sql
@@ -1,5 +1,12 @@
 select cena from filmy where rok_produkcji = 1984
-select * from 'aktorzy' 
-select * from 'filmy' where cena < 8
-select * from 
+select * from aktorzy where 1
+select * from filmy where cena < 8
+select * from filmy where cena < 2006 and cena > 8
+select * from filmy where rok_produkcji = 1984 or cena > 6
+select * from filmy where cena < 2006 and cena > 8 order by 'tytuł'
+
+kolejność klauzur w sql jest ściśle określona 
+najpierw select, from, where, order by
+
+odczytaj rekordy filmów w których pierwsza litera to 'T'
 
