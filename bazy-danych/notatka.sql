92a82ffdfeda0839fae2796d5542fc95091afc62
Mateusz B <144439963+mateuszbaguette@users.noreply.github.com>
2023-10-13 09:14:32 +0200

    Update notatka.sql

--- bazy-danych/notatka.sql
+++ bazy-danych/notatka.sql
@@ -22,3 +22,18 @@ SUBSTR() - zwaraca fragment tekstu
 */
 SELECT id_kopii, data_zwrotu-data_wypozyczenia as czas FROM wypozyczenia WHERE data_zwrotu-data_wypozyczenia>1
 SELECT CONCAT(LEFT(imie,1),'. ', nazwisko) as aktor from aktorzy 
+
+--połączenie kartezjanskie definicja, rekordy scalane na wszystkie sposoby, każdy z każdym
+SELECT * FROM tabela1 CROSS JOIN tabela2
+--połączenie naturalne 
+SELECT * FROM tabela1 NATURAL JOIN tabela2
+
+SELECT * FROM kopie join filmy on kopie.id_filmu=filmy.id_filmu;
+--wyświetl połączenie kopie i filmy, gdzie if kopi to id filmu. 
+--definicja połączenie zwrotne
+SELECT * FROM tabela1 as t1 join tabela2 as t2 on warunek_połączeniowy
+--przykład
+SELECT * FROM aktorzy as a1 join aktorzy as a2 on a1.imie=a2.imie where a1.imie = 'Tom';
+
+
+
